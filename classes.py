3617338b8d64b6ca8ec5415a14df319a2742b74f
pedro-gerhardt <pedro.lucas.gerhardt@gmail.com>
2023-11-16 18:15:31 -0300

    commit inicial

---
+++ classes.py
@@ -0,0 +1,62 @@
+import datetime
+
+class Inode:
+    # header 
+    prop = grupo = pai = None
+    dtCri = dtAtu = dtUltAce = datetime.datetime.now()
+    direitos = [bin(7), bin(0), bin(0)]
+    ehDir = False
+    tamanho = 0
+    nome = ""
+
+    # conteudo
+    blocos = []
+    indirSimp = []
+
+    def __init__(self, nome, prop, ehDir=False, pai=None):
+        self.prop = self.grupo = prop
+        self.dtCri = self.dtAtu = self.dtUltAce = datetime.datetime.now()
+        self.ehDir = ehDir
+        self.nome = nome
+        self.pai = pai
+        self.direitos = [bin(7), bin(0), bin(0)]
+        self.blocos = self.indirSimp = []
+
+    def __str__(self):
+        return f"{self.nome + '/' if self.ehDir else self.nome}"
+
+    def info(self):
+        return f"{str(self)}  {'d' if self.ehDir else 'f'}{self.normalizaDireitos()}  {self.prop.nome} {self.grupo.nome}"
+
+    def normalizaDireitos(self):
+        padrao = "rwx"
+        saida = ""
+        for dir in self.direitos:
+            strDir = str(dir)[2:].zfill(3)
+            for i in range(len(strDir)):
+                saida += f"{padrao[i] if strDir[i] == str(1) else '-'}"
+        return saida
+
+    def returnAbsolutePath(self):
+        if self.pai is not None:
+            return self.pai.returnAbsolutePath() + str(self)
+        else:
+            return str(self)
+
+    def setaDireito(self, novosDireitos):
+        self.direitos = [bin(int(i)) for i in novosDireitos]
+
+
+class User:
+    id = 0
+    nome = ""
+    senha = ""
+
+    def __init__(self, id, nome="", senha=""):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+    def __str__(self):
+        return f"{self.id} - {self.nome}"
+    
---
+++ main.py
@@ -0,0 +1,131 @@
+import datetime, os
+from classes import User, Inode
+
+# variaveis
+userRoot = User(0, "root")
+listUser = [userRoot]
+user = userRoot
+userIdInc = 1
+
+root = Inode("", userRoot, True)
+inode = root
+
+# comandos
+def pwd():
+    print(inode.returnAbsolutePath())
+
+def mkdir(cmds):
+    if len(cmds) > 1:
+        inode.blocos.append(Inode(cmds[1], user, True, inode))
+
+def touch(cmds):
+    if len(cmds) > 1:
+        inode.blocos.append(Inode(cmds[1], user, False, inode))
+
+def ls(cmds):
+    if len(cmds) == 1:
+        for sd in inode.blocos:
+            print(sd.info())
+    elif len(cmds) == 2:
+        for sd in inode.blocos:
+            if sd.nome == cmds[1]:
+                for ssd in sd.blocos:
+                    print(ssd)
+
+def cd(cmds):
+    if len(cmds) > 1:
+        if cmds[1] == ".." and inode.pai is not None:
+            inode = inode.pai
+        else:
+            for d in inode.blocos:
+                if d.nome == cmds[1]:
+                    inode = d
+
+def rm(cmds):
+    if len(cmds) > 1:
+        for d in inode.blocos:
+            if d.nome == cmds[1] and not d.ehDir:
+                inode.blocos.remove(d)
+
+def rmdir(cmds):
+    if len(cmds) > 1:
+        for d in inode.blocos:
+            if d.nome == cmds[1] and d.ehDir:
+                inode.blocos.remove(d)
+
+def adduser(cmds): 
+    if len(cmds) == 2:
+        listUser.append(User(userIdInc, cmds[1]))
+        userIdInc += 1
+    if len(cmds) == 3:
+        listUser.append(User(int(cmds[2]), cmds[1]))
+
+def rmuser(cmds): 
+    if len(cmds) > 1:
+        for u in listUser:
+            if u.nome == cmds[1]:
+                listUser.remove(u)
+
+def lsuser():
+    for u in listUser:
+        print(u)
+
+def chown(cmds):
+    if len(cmds) in [3, 4]:
+        for d in inode.blocos:
+            if d.nome == cmds[-1]:
+                for u in listUser:
+                    if u.nome == cmds[1]:
+                        d.prop = u
+                    if len(cmds) == 4 and u.nome == cmds[2]:
+                        d.grupo = u
+
+def chmod(cmds):
+    if len(cmds) == 3:
+        for d in inode.blocos:
+            if d.nome == cmds[1]:
+                d.setaDireito(cmds[2])
+                continue
+        print("Erro: Diretório/Arquivo não encontrado")
+    else:
+        print("Erro: Número de argumentos inválido")
+        
+def main():
+    global inode, user, listUser
+
+    while True:
+        comando = input(inode.returnAbsolutePath() + "# ")
+        cmds = comando.split()
+        
+        if cmds[0] == "pwd":
+            pwd()
+        elif cmds[0] == "mkdir":
+            mkdir(cmds)
+        elif cmds[0] == "touch":
+            touch(cmds)
+        elif cmds[0] == "ls":
+            ls(cmds)
+        elif cmds[0] == "cd":
+            cd(cmds)
+        elif cmds[0] == "rm":
+            rm(cmds)
+        elif cmds[0] == "rmdir":
+            rmdir(cmds)
+        elif cmds[0] == "adduser":
+            adduser(cmds)
+        elif cmds[0] == "rmuser":
+            rmuser(cmds)
+        elif cmds[0] == "lsuser":
+            lsuser()
+        elif cmds[0] == "c":
+            os.system('clear')
+        elif cmds[0] == "chown":
+            chown(cmds)
+        elif cmds[0] == "chmod":
+            chmod(cmds)
+
+
+main()
+
+
+
