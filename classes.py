2a4a5bad6f75f88f0218ab4251020fa032e59a21
pedro-gerhardt <pedro.lucas.gerhardt@gmail.com>
2023-11-20 17:45:28 -0300

    tratamento de direitos nas ações; acesso de terminal como usuario criado; tratamento e função das datas;

--- classes.py
+++ classes.py
@@ -1,77 +1,123 @@
 import datetime
+from enum import Enum
 from config import *
 
 
+def _binToStrDireito(dir):
+    return str(dir)[2:].zfill(3)
+
+
 class Inode:
-  # header
-  prop = grupo = None
-  dtCri = dtAtu = dtUltAce = datetime.datetime.now()
-  direitos = [bin(7), bin(0), bin(0)]
-  ehDir = False
-  tamanho = 0
-  absNome = ""
+    # header
+    prop = grupo = None
+    dtCri = dtAtu = dtUltAce = datetime.datetime.now()
+    direitos = [bin(7), bin(5), bin(0)]
+    ehDir = False
+    tamanho = 0
+    absNome = ""
 
-  # conteudo
-  blocos = []
-  indirSimp = []
+    # conteudo
+    blocos = []
+    indirSimp = []
 
-  def __init__(self, nome, prop, ehDir=False):
-    self.prop = self.grupo = prop
-    self.dtCri = self.dtAtu = self.dtUltAce = datetime.datetime.now()
-    self.ehDir = ehDir
-    self.absNome = nome
-    self.direitos = [bin(7), bin(0), bin(0)]
-    self.blocos = [] if self.ehDir else [bytearray(TAM_BLOCO) for _ in range(QTD_BLOCOS)]
-    self.indirSimp = []
+    def __init__(self, nome, prop, ehDir=False):
+        self.prop = self.grupo = prop
+        self.dtCri = self.dtAtu = self.dtUltAce = datetime.datetime.now()
+        self.ehDir = ehDir
+        self.absNome = nome
+        self.direitos = [bin(7), bin(5), bin(0)]
+        self.blocos = [] if self.ehDir else [bytearray(TAM_BLOCO) for _ in range(QTD_BLOCOS)]
+        self.indirSimp = []
 
-  def __str__(self):
-    return f"{self.relNome()}"
+    def __str__(self):
+        return f"{self.relNome()}"
 
-  def info(self):
-    self.atualizaTamanho()
-    return f"{str(self)}  {'d' if self.ehDir else 'f'}{self.normalizaDireitos()}  {self.tamanho}  {self.prop.nome} {self.grupo.nome}"
+    def info(self, listUser):
+        self.atualizaTamanho()
+        return f"{str(self)}  {'d' if self.ehDir else 'f'}{self.normalizaDireitos()}  {self.tamanho}  {listUser[self.prop].nome if self.prop in listUser else self.prop} {listUser[self.grupo].nome if self.grupo in listUser else self.grupo}"
 
-  def data(self):
-    return f"{str(self)}  {self.dtCri.isoformat(' ', 'seconds')} {self.dtAtu.isoformat(' ', 'seconds')} {self.dtUltAce.isoformat(' ', 'seconds')}"
+    def data(self):
+        return f"{str(self)}  {self.dtCri.isoformat(' ', 'seconds')}    {self.dtAtu.isoformat(' ', 'seconds')}    {self.dtUltAce.isoformat(' ', 'seconds')}"
 
-  def normalizaDireitos(self):
-    padrao = "rwx"
-    saida = ""
-    for dir in self.direitos:
-      strDir = str(dir)[2:].zfill(3)
-      for i in range(len(strDir)):
-        saida += f"{padrao[i] if strDir[i] == str(1) else '-'}"
-    return saida
+    def normalizaDireitos(self):
+        padrao = "rwx"
+        saida = ""
+        for dir in self.direitos:
+            strDir = _binToStrDireito(dir)
+            for i in range(len(strDir)):
+                saida += f"{padrao[i] if strDir[i] == str(1) else '-'}"
+        return saida
 
-  def setaDireito(self, novosDireitos):
-    self.direitos = [bin(int(i)) for i in novosDireitos]
+    def setaDireito(self, novosDireitos):
+        self.direitos = [bin(int(i)) for i in novosDireitos]
 
-  def formata(self):
-    self.blocos = [] if self.ehDir else [bytearray(TAM_BLOCO) for _ in range(QTD_BLOCOS)]
+    def formata(self):
+        self.blocos = [] if self.ehDir else [bytearray(TAM_BLOCO) for _ in range(QTD_BLOCOS)]
 
-  def atualizaTamanho(self):
-    self.tamanho = sum([len(b) - b.count(b"\x00") for b in self.blocos])
+    def atualizaTamanho(self):
+        self.tamanho = (
+            len(self.blocos) * 4
+            if self.ehDir
+            else sum([len(b) - b.count(b"\x00") for b in self.blocos])
+        )
 
+    def pai(self):
+        if "/" == self.absNome:
+            return None
+        ridx = self.absNome.rindex("/", 0, len(self.absNome) - 1)
+        return self.absNome[:ridx] if ridx != 0 else self.absNome[0]
+        # return self.absNome[:self.absNome.rindex("/", 0, len(self.absNome)-1)] if "/" != self.absNome else None
 
-  def pai(self):
-    if "/" == self.absNome: return None
-    ridx = self.absNome.rindex("/", 0, len(self.absNome)-1)
-    return self.absNome[:ridx] if ridx != 0 else self.absNome[0]
-    # return self.absNome[:self.absNome.rindex("/", 0, len(self.absNome)-1)] if "/" != self.absNome else None
+    def relNome(self):
+        return (
+            self.absNome[self.absNome.rindex("/", 0, len(self.absNome) - 1) + 1 :]
+            if "/" != self.absNome
+            else "/"
+        )
 
-  def relNome(self):
-    return self.absNome[self.absNome.rindex("/", 0, len(self.absNome)-1)+1:] if "/" != self.absNome else "/"
+    def verificaPermissao(self, userId, acao):
+        return (
+            (_binToStrDireito(self.direitos[Agente.GERAL.value])[acao.value] != "0")
+            or (
+                self.grupo == userId
+                and _binToStrDireito(self.direitos[Agente.GRUPO.value])[acao.value]
+                != "0"
+            )
+            or (
+                self.prop == userId
+                and _binToStrDireito(self.direitos[Agente.PROPRIETARIO.value])[acao.value]
+                != "0"
+            )
+        )
+
+    def atualizaDataAtualizacao(self):
+        self.dtUltAce = self.dtAtu = datetime.datetime.now()
+
+    def atualizaDataAcesso(self):
+        self.dtUltAce = datetime.datetime.now()
 
 
 class User:
-  id = 0
-  nome = ""
-  senha = ""
+    id = 0
+    nome = ""
+    senha = ""
 
-  def __init__(self, id, nome="", senha=""):
-    self.id = id
-    self.nome = nome
-    self.senha = senha
+    def __init__(self, id, nome="", senha=""):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
 
-  def __str__(self):
-    return f"{self.id} - {self.nome}"
+    def __str__(self):
+        return f"{self.id} - {self.nome}"
+
+
+class Acao(Enum):
+    LEITURA = 0
+    ESCRITA = 1
+    EXECUCAO = 2
+
+
+class Agente(Enum):
+    PROPRIETARIO = 0
+    GRUPO = 1
+    GERAL = 2
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from classes import User, Inode
+from classes import *
 from config import *
 
 # variaveis
@@ -7,7 +7,8 @@ userRoot = User(0, "root")
 listUser = {0:userRoot}
 user = userRoot
 
-root = Inode("/", userRoot, True)
+root = Inode("/", userRoot.id, True)
+root.direitos = [bin(7), bin(7), bin(7)]
 inode = root
 inodes = {"/": root}
 
@@ -21,50 +22,72 @@ def pwd():
 
 
 def mkdir(cmds):
+    if not inode.verificaPermissao(user.id, Acao.ESCRITA):
+        print(MSG_PERM_INS); return
     if len(cmds) != 2:
         print(MSG_QTD_INV_PARAM); return
     nome = _montaPossivelNome(cmds[1])
     if nome in inodes and inodes[nome].ehDir:
         print(MSG_DIR_EXIST); return
     else:
-        inodes[nome] = Inode(nome, user, True)
+        inodes[nome] = Inode(nome, user.id, True)
         inode.blocos.append(nome)
+        inode.atualizaDataAtualizacao()
+
 
 
 def touch(cmds):
+    if not inode.verificaPermissao(user.id, Acao.ESCRITA):
+        print(MSG_PERM_INS); return
     if len(cmds) != 2:
         print(MSG_QTD_INV_PARAM); return
     nome = _montaPossivelNome(cmds[1])
     if nome in inodes and not inodes[nome].ehDir:
         print(MSG_ARQ_EXIST); return
     else:
-        inodes[nome] = Inode(nome, user, False)
+        inodes[nome] = Inode(nome, user.id, False)
         inode.blocos.append(nome)
+        inode.atualizaDataAtualizacao()
 
 
 def ls(cmds):
     if len(cmds) not in [1,2]:
         print(MSG_QTD_INV_PARAM); return
     if len(cmds) == 1:
+        if not inode.verificaPermissao(user.id, Acao.LEITURA):
+            print(MSG_PERM_INS); return
         for sd in inode.blocos:
-            print(inodes[sd].info())
+            print(inodes[sd].info(listUser))
+        inode.atualizaDataAcesso()
     elif len(cmds) == 2:
         nome = _montaPossivelNome(cmds[1])
         if nome in inode.blocos and inodes[nome].ehDir:
-            for ssd in inodes[sd.absNome].blocos:
-                print(inodes[ssd].info())
+            if inodes[nome].verificaPermissao(user.id, Acao.LEITURA):
+                for ssd in inodes[nome].blocos:
+                    print(inodes[ssd].info(listUser))
+                inodes[nome].atualizaDataAcesso()
+            else:
+                print(MSG_PERM_INS); return
         else:
             print(MSG_DIR_NAO_EXIST); return  
-
 
 def cd(cmds):
     global inode
     if len(cmds) == 2:
         if cmds[1] == ".." and inode.pai() is not None:
-            inode = inodes[inode.pai()]; return
+            if inode.pai() not in inodes:
+                print(MSG_DIR_NAO_EXIST); return
+            if not inodes[inode.pai()].verificaPermissao(user.id, Acao.EXECUCAO):
+                print(MSG_PERM_INS); return
+            inode = inodes[inode.pai()]
+            inode.atualizaDataAcesso()
+            return
         nome = _montaPossivelNome(cmds[1])
         if nome in inode.blocos and nome in inodes and inodes[nome].ehDir:
+            if not inodes[nome].verificaPermissao(user.id, Acao.EXECUCAO):
+                print(MSG_PERM_INS); return
             inode = inodes[nome]
+            inode.atualizaDataAcesso()
         else:
             print(MSG_DIR_NAO_EXIST); return
     else:
@@ -72,23 +95,39 @@ def cd(cmds):
 
 
 def rm(cmds):
-  if len(cmds) == 2:
-    nome = _montaPossivelNome(cmds[1])
-    if nome in inode.blocos and nome in inodes and not inodes[nome].ehDir:
-        inode.blocos.remove(nome)
-        del inodes[nome]
+    if len(cmds) == 2:
+        nome = _montaPossivelNome(cmds[1])
+        if nome in inode.blocos and nome in inodes and not inodes[nome].ehDir:
+            if inodes[nome].verificaPermissao(user.id, Acao.ESCRITA):
+                inode.blocos.remove(nome)
+                del inodes[nome]
+                inode.atualizaDataAtualizacao()
+            else:
+                print(MSG_PERM_INS); return
+        else:
+            print(MSG_ARQ_NAO_EXIST); return
     else:
-      print(MSG_ARQ_NAO_EXIST); return
-  else:
-    print(MSG_QTD_INV_PARAM); return
+        print(MSG_QTD_INV_PARAM); return
 
 
 def rmdir(cmds):
     if len(cmds) == 2:
         nome = _montaPossivelNome(cmds[1])
         if nome in inode.blocos and nome in inodes and inodes[nome].ehDir:
-            inode.blocos.remove(nome)
-            del inodes[nome]
+            if inodes[nome].verificaPermissao(user.id, Acao.ESCRITA):
+                # passar por todos inodes e se no nome há o caminho do diretorio a ser excluido
+                inode.blocos.remove(nome)
+                del inodes[nome]
+                dirApagar = []
+                for i in inodes:
+                    if i.count(nome) > 0:
+                        dirApagar.append(i)
+                for i in dirApagar:
+                    del inodes[i]
+
+                inode.atualizaDataAtualizacao()
+            else:
+                print(MSG_PERM_INS); return
         else:
             print(MSG_DIR_NAO_EXIST); return
     else:
@@ -96,7 +135,11 @@ def rmdir(cmds):
 
 
 def adduser(cmds):
+    if user.id != userRoot.id:
+        print(MSG_PERM_INS); return
     if len(cmds) == 3:
+        if not cmds[2].isdigit(): 
+            print(MSG_ARG_INV); return
         if int(cmds[2]) in listUser:
             print("Usuário existente!"); return
         else:
@@ -107,7 +150,13 @@ def adduser(cmds):
 
 # remove por id
 def rmuser(cmds):
+    if user.id != userRoot.id:
+        print(MSG_PERM_INS); return
     if len(cmds) == 2:
+        if not cmds[1].isdigit(): 
+            print(MSG_ARG_INV); return
+        if int(cmds[1]) == 0:
+            print("Não é possível remover o root!"); return
         if int(cmds[1]) in listUser:
             # chamar funcao para apagar todos arquivos que usuario é proprietario
             del listUser[int(cmds[1])]
@@ -118,30 +167,39 @@ def rmuser(cmds):
 
 
 def lsuser():
+    if user.id != userRoot.id:
+        print(MSG_PERM_INS); return
     for u in listUser.values():
         print(u)
 
 
 def chown(cmds):
+    if user.id != userRoot.id:
+        print(MSG_PERM_INS); return
     if len(cmds) in [3, 4]:
         nome = _montaPossivelNome(cmds[-1])
         if nome in inode.blocos:
             for u in listUser.values():
                 if u.nome == cmds[1]:
-                    inodes[nome].prop = u
+                    inodes[nome].prop = u.id
+                    inodes[nome].atualizaDataAtualizacao()
                 if len(cmds) == 4 and u.nome == cmds[2]:
-                    inodes[nome].grupo = u
+                    inodes[nome].grupo = u.id
+                    inodes[nome].atualizaDataAtualizacao()
         else:
             print(MSG_DIR_ARQ_NAO_EXIST); return
     else:
-      print(MSG_QTD_INV_PARAM); return
+        print(MSG_QTD_INV_PARAM); return
 
 
 def chmod(cmds):
+    if user.id != userRoot.id:
+        print(MSG_PERM_INS); return
     if len(cmds) == 3:
         nome = _montaPossivelNome(cmds[1])
         if nome in inode.blocos:
             inodes[nome].setaDireito(cmds[2])
+            inodes[nome].atualizaDataAtualizacao()
         else:
             print(MSG_DIR_ARQ_NAO_EXIST); return
     else:
@@ -152,14 +210,19 @@ def grava(cmds):
     if len(cmds) != 5:
         print(MSG_QTD_INV_PARAM); return
     _, nome, posicao, nbytes, buffer = cmds
+    if not posicao.isdigit() or not nbytes.isdigit(): 
+        print(MSG_ARG_INV); return
     posicao = int(posicao)
     nbytes = int(nbytes)
     if posicao > (QTD_BLOCOS * TAM_BLOCO):
-        print("Erro: posicao maior que o tamanho do disco"); return
+        print("Erro: posição maior que o tamanho do disco."); return
     blocos = None
     nomeAbs = _montaPossivelNome(nome)
     if nomeAbs in inodes and not inodes[nomeAbs].ehDir:
-        blocos = inodes[nomeAbs].blocos
+        if inodes[nomeAbs].verificaPermissao(user.id, Acao.ESCRITA):
+            blocos = inodes[nomeAbs].blocos
+        else:
+            print(MSG_PERM_INS); return
     if blocos is None:
         print(MSG_ARQ_NAO_EXIST); return
 
@@ -172,6 +235,8 @@ def grava(cmds):
 
     if posicao + tamParcial > TAM_TOTAL:
         print("Tamanho do bloco maior que o tamanho do disco"); return
+    
+    inodes[nomeAbs].atualizaDataAtualizacao()
 
     while True:
         if posBocInit + tamParcial <= TAM_BLOCO:
@@ -190,7 +255,11 @@ def cat(cmds):
     if len(cmds) == 2:
         nome = _montaPossivelNome(cmds[1])
         if nome in inodes and not inodes[nome].ehDir:
-            print("".join(["".join([chr(b) for b in bloco]) for bloco in inodes[nome].blocos]))
+            if inodes[nome].verificaPermissao(user.id, Acao.LEITURA):
+                print("".join(["".join([chr(b) for b in bloco]) for bloco in inodes[nome].blocos]))
+                inodes[nome].atualizaDataAcesso()
+            else:
+                print(MSG_PERM_INS); return
         else:
            print(MSG_ARQ_NAO_EXIST); return
     else:
@@ -201,14 +270,56 @@ def formata(cmds):
     if len(cmds) == 2:
         nome = _montaPossivelNome(cmds[1])
         if nome in inodes:
-            inodes[nome].formata()
+            if inodes[nome].verificaPermissao(user.id, Acao.ESCRITA):
+                inodes[nome].formata()
+                inodes[nome].atualizaDataAtualizacao()
+            else:
+                print(MSG_PERM_INS); return
+        else:
+            print(MSG_ARQ_NAO_EXIST); return
+    else:
+        print(MSG_QTD_INV_PARAM); return
+    
+
+# login id
+def login(cmds):
+    global user
+    if len(cmds) == 2:
+        if not cmds[1].isdigit():
+            print(MSG_ARG_INV); return
+        if int(cmds[1]) in listUser:
+            user = listUser[int(cmds[1])]
+        else:
+            print("Usuário não encontrado!"); return
     else:
         print(MSG_QTD_INV_PARAM); return
 
 
+def lsd(cmds):
+    if len(cmds) not in [1,2]:
+        print(MSG_QTD_INV_PARAM); return
+    if len(cmds) == 1:
+        if not inode.verificaPermissao(user.id, Acao.LEITURA):
+            print(MSG_PERM_INS); return
+        for sd in inode.blocos:
+            print(inodes[sd].data())
+        inode.atualizaDataAcesso()
+    elif len(cmds) == 2:
+        nome = _montaPossivelNome(cmds[1])
+        if nome in inode.blocos and inodes[nome].ehDir:
+            if inodes[nome].verificaPermissao(user.id, Acao.LEITURA):
+                for ssd in inodes[nome].blocos:
+                    print(inodes[ssd].data())
+                inodes[ssd].atualizaDataAcesso()
+            else:
+                print(MSG_PERM_INS); return
+        else:
+            print(MSG_DIR_NAO_EXIST); return  
+
+
 def main():
   while True:
-    comando = input(inode.absNome + "# ")
+    comando = input(user.nome + ":" + inode.absNome + "# ")
     cmds = comando.split()
 
     if cmds[0] == "pwd":
@@ -243,6 +354,11 @@ def main():
         cat(cmds)
     elif cmds[0] == "formata":
         formata(cmds)
+    elif cmds[0] == "login":
+        login(cmds)
+    elif cmds[0] == "lsd":
+        lsd(cmds)
+    
     else:
        print("Comando desconhecido!")
 
